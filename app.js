a6fe80540e48a0b118658f0911aad575e9b55847
EC2 Default User <ec2-user@ip-172-31-22-136.us-west-2.compute.internal>
2019-11-18 01:55:20 +0000

    styles

--- app.js
+++ app.js
@@ -16,6 +16,30 @@ app.get("/venus", function(req, res){
     res.render("venus.html");
 });
 
+app.get("/earth", function(req, res){
+    res.render("earth.html");
+});
+
+app.get("/mars", function(req, res){
+    res.render("mars.html");
+});
+
+app.get("/jupiter", function(req, res){
+    res.render("jupiter.html");
+});
+
+app.get("/saturn", function(req, res){
+    res.render("saturn.html");
+});
+
+app.get("/uranus", function(req, res){
+    res.render("uranus.html");
+});
+
+app.get("/neptune", function(req, res){
+    res.render("neptune.html");
+});
+
 //server listener for local/cloud9
 //app.listen("8080", "127.0.0.1", function(){
 //    console.log("Express Server is Running...");
